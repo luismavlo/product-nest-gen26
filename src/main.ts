6655becc59b0e5e3d2314f7039a5cd8cc2abdeda
Luis Miguel <lmavendano1597@gmail.com>
2023-08-22 22:55:07 -0500

    hello

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+const bootstrap = async () => {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(3000);
+};
+
+bootstrap();
---
+++ src/product/dto/create-product.dto.ts
@@ -0,0 +1,27 @@
+import {
+  IsString,
+  IsNumber,
+  MinLength,
+  IsPositive,
+  IsOptional,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(3)
+  title: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  price?: number;
+
+  @IsString()
+  @MinLength(7)
+  description: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  stock?: number;
+}
