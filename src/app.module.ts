7c0be100848d7e4af63715990ef1861f5e81ec03
Luis Miguel <lmavendano1597@gmail.com>
2023-08-24 21:30:53 -0500

    next commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [ProductModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'productnestgen26',
+      username: 'postgres',
+      password: 'root',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
+    ProductModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,33 +1,27 @@
-import {
-  Injectable,
-  Inject,
-  InternalServerErrorException,
-} from '@nestjs/common';
+import { Injectable, HttpException, Logger } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class ProductService {
+  private readonly logger = new Logger('ProductService');
+
   constructor(
-    @Inject('PRODUCT_REPOSITORY')
+    @InjectRepository(Product)
     private productRepository: Repository<Product>,
   ) {}
 
   async create(createProductDto: CreateProductDto) {
     try {
-      // const product = new Product();
-      // product.title = createProductDto.title;
-      // product.description = createProductDto.description;
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save(product);
 
-      // const productCreated = this.productRepository.create(product);
-      // await this.productRepository.save(productCreated);
-
-      return 'hello';
+      return product;
     } catch (error) {
-      console.log(error);
-      // throw new InternalServerErrorException(error);
+      this.handleDBException(error);
     }
   }
 
@@ -35,15 +29,41 @@ export class ProductService {
     return this.productRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    const product = await this.productRepository.findOneBy({ id });
+
+    if (!product) throw new HttpException('Product not found', 404);
+
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    await this.findOne(id);
+
+    const productLoaded = await this.productRepository.preload({
+      id: id,
+      ...updateProductDto,
+    });
+    try {
+      await this.productRepository.save(productLoaded);
+
+      return productLoaded;
+    } catch (error) {
+      this.handleDBException(error);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    const product = await this.findOne(id);
+
+    await this.productRepository.remove(product);
+  }
+
+  private handleDBException(error: any) {
+    if (error.code === '23505')
+      throw new HttpException('Product already exists', 400);
+
+    this.logger.error(error);
+    throw new HttpException('Something went wrong', 500);
   }
 }
